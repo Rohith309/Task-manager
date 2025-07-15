d86feddbd186ce04049dc7087bf744149f5d37e8
Rohith-140 <rohith.g@station-s.org>
2025-07-15 13:24:54 +0530

    removed the taskmanager backend to push into another repo

--- taskmanager_backend/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'priority', 'status', 'deadline', 'created_at')
-    list_filter = ('priority', 'status', 'user')
-    search_fields = ('title', 'description')
-    date_hierarchy = 'deadline'
